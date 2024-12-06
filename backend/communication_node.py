d7f6a256c03e272c0bee2a1a76e87a469b8a892b
Sami Suonpää <sami.suonpaa@gmail.com>
2024-12-06 18:01:47 +0200

    Status in progress

--- backend/communication_node.py
+++ backend/communication_node.py
@@ -16,11 +16,12 @@ async def guess(letter):
     await send_info({'Letter': letter})
 
 async def send_info(information):
+    print(len(OTHER_NODES))
     for node in OTHER_NODES:
         data = json.dumps(information)
-        node[1].write(data.encode())
+        node[1].write(f'{data}\n'.encode())
         await node[1].drain()
-        response = await node[0].read(100)
+        response = await node[0].readline()
         print(response.decode())
 
 async def listen_for_connections(host, port):
@@ -31,19 +32,23 @@ async def listen_for_connections(host, port):
 
 async def handle_client(reader, writer):
     while True:
-        response = await reader.read(100)
+        response = await reader.readline()
         decoded_json = response.decode()
+        print(decoded_json)
         if "Letter" in decoded_json:
             decoded = json.loads(decoded_json)
             game.guess_letter(decoded["Letter"])
-            writer.write(b"OK")
+            writer.write(b"OK\n")
             await writer.drain()
         else:
             OTHER_NODES.append((reader, writer))
             addr = writer.get_extra_info('peername')
             print(f"Connection from {addr}")
-            await send_info(game.as_JSON())
-
+            dump = json.dumps(game.as_JSON())
+            writer.write(f'{dump}\n'.encode())
+            await writer.drain()
+            response = await reader.readline()
+            print(response.decode())
 
     
 
@@ -56,7 +61,7 @@ async def initiate_connection(target_host, target_port = "1999"):
         response = await reader.read(100)
         print(f"Received from server {target_host}: {response.decode()}")
         #TODO: decode game object and replace the current game object
-        
+        writer.write(b'OK\n')
         OTHER_NODES.append((reader,writer))
     except Exception as e:
         print(f"Failed to connect: {e}")
@@ -64,7 +69,7 @@ async def initiate_connection(target_host, target_port = "1999"):
 async def main(gameinst):
     global game
     game = gameinst
-    host = "0.0.0.0"
+    host = "192.168.68.106"
     port = 1999
     print(game.as_JSON())
     game.add_player(Player(host, 1))
