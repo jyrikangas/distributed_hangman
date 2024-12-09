95859dd70ed9b8259a212c12e0557038506f1cdf
Sami Suonpää <sami.suonpaa@gmail.com>
2024-12-09 21:45:46 +0200

    Refactor communication + add logger

--- backend/communication_node.py
+++ backend/communication_node.py
@@ -1,6 +1,6 @@
 import asyncio
 import json
-
+import time
 import os
 from dotenv import load_dotenv
 
@@ -12,22 +12,28 @@ OTHER_NODES = []
 tasks = []
 game = None
 
-async def state():
-    await send_info(game.as_JSON())
+def logger(message):
+    with open("log.txt", "a") as file:
+        file.write(f'{time.time()}:{message}')
+
+async def state(target=OTHER_NODES):
+    await send_info({'Command':'State', 'State': game.as_JSON()}, target)
 
 async def players():
     info = game.get_players()
     await send_info({'Players': info})
 
 async def guess(letter):
-    await send_info({'Letter': letter})
+    await send_info({'Command':'Guess','Letter': letter})
 
-async def send_info(information):
+async def send_info(information, target=OTHER_NODES):
     print(len(OTHER_NODES))
-    for node in OTHER_NODES:
+    for node in target:
         data = json.dumps(information)
         node[1].write(f'{data}\n'.encode())
         await node[1].drain()
+        addr = node.get_extra_info('peername')
+        logger(f'Message out {information} to {addr}')
         #response = await node[0].readline()
         #print(response.decode())
 
@@ -43,57 +49,51 @@ async def handle_client(reader, writer):
     while True:
         response = await reader.readline()
         decoded_json = response.decode()
+        logger(f'Message in {decoded_json}')
         print("decoded_json:", decoded_json)
-        if "Letter" in decoded_json:
-            decoded = json.loads(decoded_json)
+        decoded = json.loads(decoded_json)
+        if "Guess" == decoded["Command"]:
             game.guess_letter(decoded["Letter"])
-            writer.write(b"OK\n")
-            await writer.drain()
-        if "OK" in decoded_json:
+            await send_info({'Command':'Response OK'})
+
+        if "Response OK" == decoded["Command"]:
             print("OK")
-        if "board" in decoded_json:
+
+        if "State" == decoded["Command"]:
             print("State")
-        if "Hello" in decoded_json:
+            ips = []
+            sender = reader.get_extra_info('peername')
+            for player in game.get_players():
+                ips.append(player.get_ip())
+            for player in decoded["players"]:
+                if player["ip"] == sender:
+                    game.add_player(Player(sender,player["id"]))
+                    ips.append(sender)
+                if player["ip"] not in ips:
+                    game.add_player(Player(player["ip"],player["id"]))
+                    await initiate_connection(player["ip"])
+            for letter in decoded["guessed_letters"]:
+                game.guess_letter(letter)
+
+        if "Connect" == decoded["Command"]:
             OTHER_NODES.append((reader, writer))
             addr = writer.get_extra_info('peername')
             print(f"Connection from {addr}")
-            dump = json.dumps(game.as_JSON())
-            writer.write(f'{dump}\n'.encode())
-            await writer.drain()
-            response = await reader.readline()
-            print(response.decode())
             game.add_player(Player(addr[0],len(game.get_players())+1))
-
+            await state(game.as_JSON(), [writer])
+            
     
 
 async def initiate_connection(target_host, target_port = "1999"):
     try:
         reader, writer = await asyncio.open_connection(target_host, target_port)
         print(f"Connected to {target_host}:{target_port}")
-        writer.write(b"Hello, server!\n")
-        await writer.drain()
-        response = await reader.readline()
-        print(f"Received from server {target_host}: {response.decode()}")
-        #TODO: decode game object and replace the current game object
+        
+        await send_info({'Command': 'Connect'})
         OTHER_NODES.append((reader, writer))
-        writer.write(b'OK\n')
         tasks.append(asyncio.create_task(handle_client(reader, writer)))
-        decoded_json = response.decode()
-        decoded = json.loads(decoded_json)
-        ips = []
-        for player in game.get_players():
-            ips.append(player.get_ip())
-        for player in decoded["players"]:
-            if player["ip"] == target_host:
-                game.add_player(Player(target_host,player["id"]))
-                ips.append(target_host)
-            if player["ip"] not in ips:
+        
 
-                game.add_player(Player(player["ip"],player["id"]))
-                await initiate_connection(player["ip"])
-        for letter in decoded["guessed_letters"]:
-            game.guess_letter(letter)
-            print(player)
     except Exception as e:
         print(f"Failed to connect: {e}")
 
