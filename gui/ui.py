fbdf169d409f9e4e762f3cde2e210baf4c68107c
Jyri Kangas <jyri_kangas@outlook.com>
2024-12-11 21:55:11 +0200

    send the whole state when guessing

--- gui/ui.py
+++ gui/ui.py
@@ -136,7 +136,7 @@ class UI:
                             all_letters_found = False
                     print("all_letters_found in the end:", all_letters_found)
                     
-                    await communication.guess(letter)
+                    await communication.state(game)
                             
                             # jos true, tee jotain napille - no need to change button?
                             #LETTER_BUTTONS.remove([button, letter])
